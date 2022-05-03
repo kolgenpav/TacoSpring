0a2997ac5abb50dd238e6de7446ae86bfeab6db6
Hennadii Kolomoiets <kolgenpav@gmail.com>
2022-05-03 19:33:01 +0300

    Initial cpmmit of Taco-Cloud app with home controller, page and test (after chapter 1).

---
+++ src/main/java/com/example/tacos/TacoCloudApplication.java
@@ -0,0 +1,18 @@
+package com.example.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * To run the application type
+ * mvn spring-boot:run
+ * in console when in root app directory.
+ */
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
